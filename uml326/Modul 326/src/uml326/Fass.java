07ece8d72a643915db9f16370d5c4beef052ea0b
Özgür Isbert <oezguerisbert@gmail.com>
2020-10-28 09:49:23 +0100

    updates

--- uml326/Modul 326/src/uml326/Fass.java
+++ uml326/Modul 326/src/uml326/Fass.java
@@ -2,12 +2,19 @@ package uml326;
 
 public class Fass extends Behaelter {
     
+    //Eigenschaften
+    double hoehe;
+    double radius;
 
+    public Fass(double h, double r) {
+        this.hoehe = h;
+        this.radius = r;
+    }
 
     @Override
     double getVolumen() {
         
-        return 0;
+        return this.radius * this.radius * Math.PI * hoehe;
     }
     
 }
