b1bb0898040d9fd8fa56f9608e0955e93c9f7963
Özgür Isbert <oezguerisbert@gmail.com>
2020-10-28 08:41:53 +0100

    created HelloWorld

---
+++ uml326/HelloWorld.java
@@ -0,0 +1,11 @@
+package uml326;
+
+//** @class HelloWorld */
+public class HelloWorld {
+    
+    //** Main Methode */
+    public static void main(String[] args){
+        System.out.println("Hello World, Hallo Basel!");
+    }
+
+}
