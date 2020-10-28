6c7d41697c236dc84561a52f0ba841be456ff1d0
Özgür Isbert <oezguerisbert@gmail.com>
2020-10-28 10:33:03 +0100

    updates

--- uml326/Modul 326/src/uml326/Tester.java
+++ uml326/Modul 326/src/uml326/Tester.java
@@ -3,10 +3,10 @@ package uml326;
 public class Tester {
     public static void main(String[] args) {
         Kiste k = new Kiste(10,10,10);
-        System.out.println(k.getVolumen());
+        // System.out.println(k.getVolumen());
 
         Fass f = new Fass(1,2);
-        System.out.println(f.getVolumen());
+        // System.out.println(f.getVolumen());
 
         LKWLadung lkwlad = new LKWLadung();
         lkwlad.dazu(k);
