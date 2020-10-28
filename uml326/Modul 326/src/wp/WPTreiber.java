2a901f92bd41f404794c4f65f218d28337f54d23
Özgür Isbert <oezguerisbert@gmail.com>
2020-10-28 13:32:44 +0100

    main method updated

--- uml326/Modul 326/src/wp/WPTreiber.java
+++ uml326/Modul 326/src/wp/WPTreiber.java
@@ -1,5 +1,11 @@
 package wp;
 
 public class WPTreiber {
-    
+    public static void main(String[] args) {
+        Aktie ubsn =  new Aktie("UBS Name", "UBSN", 10.65);
+
+        System.out.println(ubsn.getWert());
+
+
+    }
 }
