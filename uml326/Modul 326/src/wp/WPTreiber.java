7ca814b3342b96133e84e3fa01660289d8dc63f1
Özgür Isbert <oezguerisbert@gmail.com>
2020-10-29 08:57:22 +0100

    added tester for WertschriftenDepot

--- uml326/Modul 326/src/wp/WPTreiber.java
+++ uml326/Modul 326/src/wp/WPTreiber.java
@@ -4,13 +4,17 @@ public class WPTreiber {
     public static void main(String[] args) {
         Aktie ubsn =  new Aktie("UBS Name", "UBSN", 10.65);
 
-        System.out.println(ubsn.getWert());
+        System.out.println("USBN wert:" + ubsn.getWert());
 
         Obligation wien32 = new Obligation("wien32", "w32", 103.25, 100000.0, 1.25);
-        System.out.println(wien32.getWert());
+        System.out.println("Wien32 wert: " + wien32.getWert());
 
 
         WertpapierPacket wpp = new WertpapierPacket(ubsn, 1000, "8.3.2018", 13.27);
-        System.out.println(wpp.getWert());
+        System.out.println("WertpapierPacket UBSN wert: " + wpp.getWert());
+
+        WertschriftenDepot wsd = new WertschriftenDepot();
+        wsd.dazu(wpp);
+        System.out.println("WertschriftenDepot wert: " + wsd.getWert());
     }
 }
