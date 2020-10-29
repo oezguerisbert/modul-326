ca84aa88cd5c0f38e923195333ceb259a8cf71c4
Özgür Isbert <oezguerisbert@gmail.com>
2020-10-29 15:47:28 +0100

    added testcase

--- autoverleih/Autoverleih/src/view_controller/Autoverleih.java
+++ autoverleih/Autoverleih/src/view_controller/Autoverleih.java
@@ -1,7 +1,22 @@
 package  view_controller;
 
+import model.*;
+
 public class Autoverleih {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        PW my5PW = new PW(5);
+        PW my2PW = new PW(2);
+
+        Lieferwagen myLF2500 = new Lieferwagen(2500);
+
+        Ausleihe d3pw5 = new Ausleihe(my5PW, 3);
+        Ausleihe d2pw2 = new Ausleihe(my2PW, 2);
+        Ausleihe d7lf2500 = new Ausleihe(myLF2500, 7);
+        AusleiheListe mylist = new AusleiheListe();
+        mylist.dazu(d3pw5);
+        mylist.dazu(d2pw2);
+        mylist.dazu(d7lf2500);
+        System.out.println("Umsatz total: " + mylist.getUmsatz());
+
     }
 }
